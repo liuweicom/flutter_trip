83870d222accf8cb15ab5a59fa3990a92cfe277f
liuweicom <15779712623@163.com>
2019-07-29 09:37:32 +0800

    我也页面，打开携程页面

--- lib/page/my_page.dart
+++ lib/page/my_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_trip/widget/webview.dart';
 class MyPage extends StatefulWidget {
   @override
   _MyPageState createState() => _MyPageState();
@@ -7,6 +8,13 @@ class MyPage extends StatefulWidget {
 class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: WebView(
+        url: 'https://m.ctrip.com/webapp/myctrip/',
+        hideAppBar: true,
+        backForbid: true,
+        statusBarColor: '4c5bca',
+      ),
+    );
   }
 }
