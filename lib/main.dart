7a2bbfb2b1072bc18ba79627234e0a41e354145b
liuweicom <15779712623@163.com>
2019-07-30 11:18:52 +0800

    打包配置

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter trip',
+      title: 'flutter之旅',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
